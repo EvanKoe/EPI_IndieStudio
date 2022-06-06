d2435f7dd424cc273cdc178236017c95732ad675
cedric ulrich <cedric.ulrich@epitech.eu>
2022-06-06 14:40:35 +0200

    map generator first test

---
+++ src/map/ErrorMap.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** EPI_IndieStudio
+** File description:
+** ErrorMap
+*/
+
+#pragma once
+
+class ErrorMap {
+    public:
+        ErrorMap();
+        ~ErrorMap();
+
+    protected:
+    private:
+};
--- src/map/Map.hpp
+++ src/map/Map.hpp
@@ -23,28 +23,13 @@ enum Difficulty {
 
 class Map {
     public:
-        // If no save
-        // Map(Difficulty d = EASY) {
-        //     switch (d) {
-        //         case (EASY): _size.x = 20; _size.y = 10; break;
-        //         case (MEDIUM): _size.x = 30; _size.y = 20; break;
-        //         default: _size.x = 50; _size.y = 30; break;
-        //     }
-        //  }
-
-        // If save
-        // Map(std::list<std::list<int>> &map): _map(map) {};
         Map() {};
         void Generator(size_t x, size_t y); // Generate a map in a txt file with a random map
         void LoadMap(std::string path); // Load a map from a txt file
         void SaveMap(std::string path); // Save a map in a txt file
         void ParseMap(std::string path); // Parse a map from a txt file
         ~Map() {};
-        // return en double tableau (protected) Cédric Kélian
-        // class error sans std::exception (pas dans classe map) Cedric Kélian
-
     protected:
         Vector2 _size;
-        std::vector<std::string> _map;
-    private:
+        char[33][33] _map;
 };
