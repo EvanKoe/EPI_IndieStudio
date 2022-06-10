daddf02a18e954ce13593b59a44d7270edbf4a9a
ankidets <hugo.kirch@epitech.eu>
2022-06-10 21:56:55 +0200

    [ADD] Player Class

---
+++ src/game control/Player/Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ankidets on 10/06/22.
+//
+
+#include "Player.hpp"
---
+++ src/game control/Player/Player.hpp
@@ -0,0 +1,11 @@
+//
+// Created by ankidets on 10/06/22.
+//
+
+#ifndef BOMBERMAN_PLAYER_HPP
+#define BOMBERMAN_PLAYER_HPP
+
+class Player {
+};
+
+#endif //BOMBERMAN_PLAYER_HPP
